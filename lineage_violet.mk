878ffcc1f2ad830c944fb5aebec1af5b0c168f7c
Chronix <chroniceditz99@gmail.com>
2026-01-05 13:31:45 +0530

    AviumUI Bringups

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -11,19 +11,13 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit MiuiCamera
+# Inherit MemeCamera
 $(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
 
-# AxionAOSP
-AXION_CAMERA_REAR_INFO := 48,5
-AXION_CAMERA_FRONT_INFO := 13
-AXION_MAINTAINER := Joker-V2
-AXION_PROCESSOR := Qualcomm_Snapdragon_675
-GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/available_frequencies
-GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
-PERF_GOV_SUPPORTED := true
-PERF_DEFAULT_GOV := schedutil
-TARGET_ENABLE_BLUR := true
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
@@ -33,3 +27,6 @@ PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#AviumUI
+-include device/xiaomi/violet/avium_vio.mk
