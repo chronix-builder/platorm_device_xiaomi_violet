bcec134343a6a2ad95ce8142a7c3da6a26126c92
Chronix <chroniceditz99@gmail.com>
2026-01-05 13:34:09 +0530

    Initial AviumUI Config
    Create avium_vio.mk & added AviumUI flags

---
+++ avium_vio.mk
@@ -0,0 +1,25 @@
+#
+# SPDX-FileCopyrightText: 2025 The AviumUI Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+AVIUM_VERSION_APPEND_TIME_OF_DAY := false
+
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# GMS
+WITH_GMS := true
+TARGET_GMS_TYPE := MINI
+
+# LatinIMEGooglePrebuilt
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Spoof
+AVIUM_FORCE_SET_FAKE_PROP := true
+
+# Maintainer
+AVIUM_MAINTAINER := Chronix
